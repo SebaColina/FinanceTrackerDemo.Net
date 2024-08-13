9c0f5ce6a76e0821c69ceceb9fd710b5476bb043
Seba <sebacoli00@gmail.com>
2024-08-13 15:35:00 -0300

    repository, mapping and other files setup
    Signed-off-by: Seba <sebacoli00@gmail.com>

--- finance_tracker_backend/DTOs/TransactionDto.cs
+++ finance_tracker_backend/DTOs/TransactionDto.cs
@@ -1,9 +1,9 @@
 public class TransactionDto
 {
     public int Id { get; set; }
-    public int Amount { get; set; }
-    public DateOnly Date { get; set; }
-    public string Description { get; set; }
-    public string Category { get; set; }
-    public string UserId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public string? UserId { get; set; }
 }
--- finance_tracker_backend/Model/TransactionModel.cs
+++ finance_tracker_backend/Model/TransactionModel.cs
@@ -12,10 +12,10 @@ public class Transaction
     public DateTime Date { get; set; }
 
     [Required]
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     [Required]
-    public string Category { get; set; }
+    public string? Category { get; set; }
 
-    public string UserId { get; set; }
+    public string? UserId { get; set; }
 }
